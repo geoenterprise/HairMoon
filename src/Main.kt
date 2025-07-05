b25fa84ad5ded9fd743b1e1912436c3f82bdac5f
geoenterprise <jovannyrey@hotmail.com>
2025-07-04 23:51:54 -0500

    first commit after cloning

--- src/Main.kt
+++ src/Main.kt
@@ -7,9 +7,5 @@ fun main() {
     println("Hello, $name!")
     println("\uD83C\uDF19 Welcome to HairMoon – Haircut Planning by the Moon!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+
 }
